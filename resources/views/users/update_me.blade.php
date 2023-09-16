e10cf4e35d4489d17e8d86d3f80af43fd2a38192
Raven-312 <bryanhk18@gmail.com>
2023-09-16 01:38:44 -0400

    Correccion de titulos del admintemplate

--- resources/views/users/update_me.blade.php
+++ resources/views/users/update_me.blade.php
@@ -148,7 +148,7 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h2><i class="fas fa-circle-question"></i> Genesis-Lite</h2>
+                <h2><i class="fas fa-circle-question"></i>Textiles Nueva York</h2>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body text-center">
